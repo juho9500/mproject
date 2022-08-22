bc8df8fefd71036ace03b44fc38f20a3f8b989bc
yeonda21 <yeonlee21@naver.com>
2022-08-22 14:43:29 +0900

    swagger, security설정 추가 2:43

--- mproject01/src/main/java/com/example/demo/Mproject01Application.java
+++ mproject01/src/main/java/com/example/demo/Mproject01Application.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Mproject01Application {
@@ -10,4 +13,10 @@ public class Mproject01Application {
 		SpringApplication.run(Mproject01Application.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+	}
+	
+	
 }
