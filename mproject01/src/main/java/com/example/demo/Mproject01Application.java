3f06a80a93aa5895f048f541278d0c286f1b1182
yeonda21 <yeonlee21@naver.com>
2022-08-22 09:59:57 +0900

    포로젝트 최초 공유

---
+++ mproject01/src/main/java/com/example/demo/Mproject01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mproject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mproject01Application.class, args);
+	}
+
+}
